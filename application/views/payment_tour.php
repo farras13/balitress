c618e2519e6c7f6adc89d985e949c8ea6fb14002
naufal farras <naufal.farras@polytechnic.astra.ac.id>
2024-07-05 05:27:41 +0700

    imagepaytour

--- application/views/payment_tour.php
+++ application/views/payment_tour.php
@@ -14,7 +14,7 @@
             <div class="row">
                 <div class="col-md-12 mb-4">
                     <div class="card package-card">
-                        <img src="<?= $pesan->Thumbnail ?>" class="card-img-top" alt="<?= $pesan->Name ?>" width="100%" height="380px">
+                        <img src="<?= base_url($pesan->Thumbnail) ?>" class="card-img-top" alt="<?= $pesan->Name ?>" width="100%" height="380px">
                         <div class="card-body">
                             <h5 class="card-title"><?= $pesan->Name ?></h5>
                             <p>Tanggal : <?= $tanggal ?> <br> Person : <?= $qty ?></p>
@@ -62,7 +62,7 @@
         <div class="row">
                 <div class="col-md-12 mb-4">
                     <div class="card package-card">
-                        <img src="<?= $pesan->Thumbnail ?>" class="card-img-top" alt="<?= $pesan->Name ?>" width="100%" height="380px">
+                        <img src="<?= base_url($pesan->Thumbnail) ?>" class="card-img-top" alt="<?= $pesan->Name ?>" width="100%" height="380px">
                         <div class="card-body">
                             <h5 class="card-title"><?= $pesan->Name ?></h5>
                             <p>Tanggal : <?= $tanggal ?> <br> Person : <?= $qty ?></p>                            
